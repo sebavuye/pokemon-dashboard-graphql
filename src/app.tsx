e12c160a3b42e8eaffdbb5478d0a8d3eed84d78b
sebastianvuye <sebastian.vuye@gmail.com>
2023-12-28 22:28:12 +0100

    feat: add app bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
 import { useQuery } from "@apollo/client";
 import { gql } from "./__generated__";
+import CatchingPokemonTwoToneIcon from "@mui/icons-material/CatchingPokemonTwoTone";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const GET_POKEMONS = gql(`
 query pokemons($limit: Int, $offset: Int) {
@@ -22,6 +27,17 @@ function App() {
 
   return (
     <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Box display="flex" gap={1} alignItems="center">
+            <CatchingPokemonTwoToneIcon />
+            <Typography variant="h6" component="h1">
+              Pokemon Dashboard
+            </Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
       {data?.pokemons?.results?.map((pokemon) => {
         return <div key={pokemon?.name}>{pokemon?.name}</div>;
       })}
