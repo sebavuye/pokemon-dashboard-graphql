c87cac40cdf95121ecc7de1444c88afacbe03af0
sebastianvuye <sebastian.vuye@gmail.com>
2023-12-30 12:47:40 +0100

    feat: add loading skeletons

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import Skeleton from "@mui/material/Skeleton";
 import { Header } from "./features";
 
 const GET_POKEMONS = gql(`
@@ -23,13 +24,21 @@ query pokemons($limit: Int, $offset: Int) {
   }
 }`);
 
+const LIMIT = 24;
+
+const skeletons = Array.from({ length: LIMIT }, (_, key) => key);
+
 function App() {
-  const { data } = useQuery(GET_POKEMONS, { variables: { limit: 24, offset: 0 } });
+  const { data, loading } = useQuery(GET_POKEMONS, { variables: { limit: LIMIT, offset: 0 } });
 
   return (
     <div>
       <Header />
       <Box display="grid" gridTemplateColumns="repeat(4, 1fr)" gap={2} p={2}>
+        {loading &&
+          skeletons?.map((skeleton) => {
+            return <Skeleton key={skeleton} variant="rounded" height={100} />;
+          })}
         {data?.pokemons?.results?.map((pokemon) => (
           <Card key={pokemon?.name} sx={{ display: "flex" }}>
             <CardMedia component="img" image={pokemon?.image ?? undefined} alt={pokemon?.name ?? undefined} sx={{ maxHeight: "100px", objectFit: "contain" }} />
