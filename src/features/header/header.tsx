de264b0f846f46b51a41b18328d498737b60b940
sebastianvuye <sebastian.vuye@gmail.com>
2024-01-08 20:12:11 +0100

    feat: add link to logo in header

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -3,12 +3,13 @@ import Box from "@mui/material/Box";
 import CatchingPokemonTwoToneIcon from "@mui/icons-material/CatchingPokemonTwoTone";
 import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Box display="flex" gap={1} alignItems="center">
+        <Box to="/" component={Link} display="flex" gap={1} alignItems="center" color="white">
           <CatchingPokemonTwoToneIcon />
           <Typography variant="h6" component="h1">
             Pokemon Dashboard
